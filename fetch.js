8d482584d0e1e73c292b66a520e245241d6940f0
Anders Fischer <andersfischern@me.com>
2019-06-23 16:05:26 +0800

    Implemented paging if issue count is +100.

--- fetch.js
+++ fetch.js
@@ -3,10 +3,11 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 require('dotenv').config();
 
-let checkedRepositories = []
+let checkedRepositories = {};
 const makeRequest = (issueURL) => {
   const headers = {
-    Authorization: 'Basic ' +
+    Authorization:
+      'Basic ' +
       Buffer.from(
         'andersfischernielsen' + ':' + process.env.PASS,
         'utf8',
@@ -15,7 +16,8 @@ const makeRequest = (issueURL) => {
   };
 
   request(
-    issueURL, {
+    issueURL,
+    {
       json: true,
       headers: headers,
     },
@@ -31,12 +33,41 @@ const makeRequest = (issueURL) => {
           const stats = {
             name: name,
             issues: issues.length,
-            labelled: issues.filter(i => i.labels && i.labels.length > 0).length,
+            labelled: issues.filter((i) => i.labels && i.labels.length > 0)
+              .length,
           };
-          checkedRepositories.push(stats)
-          const sorted = checkedRepositories.filter(i => i.issues >= 100 && i.labelled >= 50)
-          fs.writeFileSync(`fetched_issues/checked.yaml`, yaml.safeDump(checkedRepositories));
-          fs.writeFileSync(`fetched_issues/statistics.yaml`, yaml.safeDump(sorted));
+          if (!checkedRepositories[stats.name])
+            checkedRepositories[stats.name] = stats;
+          else {
+            checkedRepositories[stats.name].issues += stats.issues;
+            checkedRepositories[stats.name].labelled += stats.labelled;
+          }
+          checkedRepositories[stats.name].percent =
+            (checkedRepositories[stats.name].labelled /
+              checkedRepositories[stats.name].issues) *
+            100;
+
+          const sorted = Object.fromEntries(
+            Object.entries(checkedRepositories).filter(
+              ([_, v]) => v.issues >= 100 && v.percent >= 65,
+            ),
+          );
+          fs.writeFileSync(
+            `fetched_issues/checked.yaml`,
+            yaml.safeDump(checkedRepositories),
+          );
+          fs.writeFileSync(
+            `fetched_issues/statistics.yaml`,
+            yaml.safeDump(sorted),
+          );
+
+          if (issues.length == 100) {
+            const pagestart = issueURL.indexOf('page=');
+            const page = +issueURL.slice(pagestart + 5, issueURL.length)[0] + 1;
+            const url =
+              issueURL.slice(0, pagestart + 5) + page + 'per_page=100';
+            makeRequest(url);
+          }
         } catch (err) {
           console.error(err);
         }
@@ -46,14 +77,14 @@ const makeRequest = (issueURL) => {
 };
 
 fs.readFile('fetched_issues/checked.yaml', (err, content) => {
-  const loaded = yaml.safeLoad(content)
-  checkedRepositories = loaded && loaded != "undefined" ? loaded.map(f => f.name) : [];
+  const loaded = yaml.safeLoad(content);
+  checkedRepositories = loaded && loaded != 'undefined' ? loaded : {};
 
   request(
     'https://raw.githubusercontent.com/ros/rosdistro/master/melodic/distribution.yaml',
     (err, res, body) => {
       const loaded = yaml.safeLoad(body);
-      const repositories = loaded.repositories;
+      let repositories = loaded.repositories;
 
       for (let i = 0; i < checkedRepositories.length; i++) {
         const checked = checkedRepositories[i];
@@ -68,7 +99,7 @@ fs.readFile('fetched_issues/checked.yaml', (err, content) => {
         if (url.match('bitbucket')) continue;
         const split = url.split(/\.|github.com/);
         split.splice(1, 0, 'api.github.com/repos');
-        split[3] = '/issues?state=all&per_page=200';
+        split[3] = '/issues?state=all&page=1&per_page=100';
         const issueURL = split.join('');
         setTimeout(() => makeRequest(issueURL), timeout);
         timeout += 1000;
